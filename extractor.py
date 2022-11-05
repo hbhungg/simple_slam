365f8ebfe1654ea72108565f7b077666f712a28d
hung <bahung1tt@gmail.com>
2022-11-05 11:11:39 +1100

    some comments

--- extractor.py
+++ extractor.py
@@ -9,8 +9,7 @@ class Extractor:
 
   def extract_feature(self, img):
     # Detect
-    # goodFeaturesToTrack required greyscale image
-    grey_img = np.mean(img, axis=2).astype(np.uint8)
+    grey_img = np.mean(img, axis=2).astype(np.uint8) # goodFeaturesToTrack required greyscale image
     pts = cv2.goodFeaturesToTrack(grey_img, 3000, qualityLevel=0.01, minDistance=7)
 
     # Extract
@@ -23,22 +22,23 @@ class Extractor:
     if self.last is not None:
       matches = self.bf.knnMatch(des, self.last['des'], k=2)
 
-      pts1 = []
-      pts2 = []
+      # Lowe's ratio test
+      pts1, pts2 = [], []
       for m,n in matches:
         if m.distance < 0.75*n.distance:
           pts1.append(kps[m.queryIdx].pt)
           pts2.append(self.last['kps'][m.trainIdx].pt)
 
+      # Filters
       pts1 = np.int32(pts1)
       pts2 = np.int32(pts2)
       F, mask = cv2.findEssentialMat(pts1, pts2)
       pts1 = pts1[mask.ravel()==1]
       pts2 = pts2[mask.ravel()==1]
-      good = zip(pts1, pts2)
+      ret = zip(pts1, pts2)
     else:
-      good = None
+      ret = None
 
     # Save last frame stuff
     self.last = {"img": img, "kps": kps, "des": des}
-    return kps, des, good
+    return kps, des, ret
